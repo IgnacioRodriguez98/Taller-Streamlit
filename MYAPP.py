20637b70f6a89c8a9d9b43a515cd88984121526d
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 14:51:15 -0700

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -7,12 +7,40 @@ st.title("Mi primer app")
 st.write(""" # My first app 
 Hello *world!*""")
 
-st.latex("\int_1^6 sin(x)dx")
-if st.button("No hacer clic"):
-    st.write("Te dije que no lo hicieras")
-else:
-    st.write("Gracias por no hacer clic")
+click = st.button("Clic")
+st.write("El valor de clic es: ",click)
 
-st.button("Entrar a los recursos")
+if click == True:
+    st.image("https://www.elsoldetlaxcala.com.mx/incoming/j3sdgs-perrito.jpg/ALTERNATES/LANDSCAPE_1140/perrito.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#st.latex("\int_1^6 sin(x)dx")
+#if st.button("No hacer clic"):
+#    st.write("Te dije que no lo hicieras")
+#else:
+#    st.write("Gracias por no hacer clic")
+
+#st.button("Entrar a los recursos")
 #st.write(df)
 #st.map(df)
+#st.markdown("""# Este es un titulo
+#""")
