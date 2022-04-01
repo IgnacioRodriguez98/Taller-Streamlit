965eeb11ed1a9a17325e8a19a571347755e9d54c
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 14:32:23 -0700

    comentado

--- MYAPP.py
+++ MYAPP.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
-st.title("Mi primer app")
-st.write(""" # My first app 
-Hello *world!*
-""")
+#st.title("Mi primer app")
+#st.write(""" # My first app 
+#Hello *world!*
+#""")
+
 if st.button("No hacer clic"):
     st.write("Te dije que no lo hicieras")
 else:
     st.write("Gracias por no hacer clic")
 
 st.button("Entrar a los recursos")
-st.write(df)
-st.map(df)
+#st.write(df)
+#st.map(df)
