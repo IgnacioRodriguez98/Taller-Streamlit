a3fbf1948771baedd1cce50543dc692472d63e7b
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 14:14:39 -0700

    AS

--- MYAPP.py
+++ MYAPP.py
@@ -3,6 +3,9 @@ st.title("Mi primer app")
 st.write(""" # My first app 
 Hello *world!*
 """)
-st.button("No hacer clic")
+if st.button("No hacer clic"):
+    st.write("Te dije que no lo hicieras")
+else:
+    st.write("Gracias por no hacer clic")
 
 st.button("Entrar a los recursos")
