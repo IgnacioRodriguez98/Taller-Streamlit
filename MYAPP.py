558ad7fecc44e30d43ef61c9581eb3c70fc4a82b
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 15:08:01 -0700

    asda

--- MYAPP.py
+++ MYAPP.py
@@ -14,16 +14,17 @@ if click == True:
     st.image("https://www.elsoldetlaxcala.com.mx/incoming/j3sdgs-perrito.jpg/ALTERNATES/LANDSCAPE_1140/perrito.jpg")
 
 ##################################Uso de Slider ##########################
-st.write(""" # Calculadora
-## Suma""")
-num1 = st.slider("Escoge el primer número",0.0,300.0,15.0)
-num2 = st.slider("Escoge el segundo número",0.0,300.0,15.0)
-st.write("La suma de esos números es:", num1+num2)
-st.write("## Multiplicación")
-num3 = st.number_input("Escribe el primer número")
-num4 = st.number_input("Escribe el segundo número")
+with st.sidebar:
+    st.write(""" # Calculadora
+    ## Suma""")
+    num1 = st.slider("Escoge el primer número",0.0,300.0,15.0)
+    num2 = st.slider("Escoge el segundo número",0.0,300.0,15.0)
+    st.write("La suma de esos números es:", num1+num2)
+    st.write("## Multiplicación")
+    num3 = st.number_input("Escribe el primer número")
+    num4 = st.number_input("Escribe el segundo número")
 
-st.write("La multiplicación de esos números es:", num3*num4)
+    st.write("La multiplicación de esos números es:", num3*num4)
 
 
 
