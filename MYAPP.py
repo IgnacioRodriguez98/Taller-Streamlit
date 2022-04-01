c14c4bf20ad9d3016584cc817231b802c1b2ae09
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 14:58:25 -0700

    suma

--- MYAPP.py
+++ MYAPP.py
@@ -2,18 +2,22 @@ import streamlit as st
 import pandas as pd
 
 #df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
-
+########################### Uso de titulo y write#############################################
 st.title("Mi primer app")
 st.write(""" # My first app 
 Hello *world!*""")
-
+##################################Uso de Boton ################################################
 click = st.button("Clic")
 st.write("El valor de clic es: ",click)
 
 if click == True:
     st.image("https://www.elsoldetlaxcala.com.mx/incoming/j3sdgs-perrito.jpg/ALTERNATES/LANDSCAPE_1140/perrito.jpg")
 
+##################################Uso de Slider ##########################
+num1 = st.slider("Escoge el primer número",0,300,15)
+num2 = st.slider("Escoge el primer número",0,300,15)
 
+st.write("La suma de esos números es:", num1+num2)
 
 
 
