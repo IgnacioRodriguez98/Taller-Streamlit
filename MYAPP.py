47125b9dbfa74e2f3056a03f9ad98cc7deeb9ea9
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-01 15:04:44 -0700

    asdsa

--- MYAPP.py
+++ MYAPP.py
@@ -14,12 +14,12 @@ if click == True:
     st.image("https://www.elsoldetlaxcala.com.mx/incoming/j3sdgs-perrito.jpg/ALTERNATES/LANDSCAPE_1140/perrito.jpg")
 
 ##################################Uso de Slider ##########################
-st,write(""" # Calculadora
+st.write(""" # Calculadora
 ##Suma""")
 num1 = st.slider("Escoge el primer número",0.0,300.0,15.0)
 num2 = st.slider("Escoge el segundo número",0.0,300.0,15.0)
 st.write("La suma de esos números es:", num1+num2)
-st,write("## Multiplicación")
+st.write("## Multiplicación")
 num3 = st.number_input("Escribe el primer número")
 num4 = st.number_input("Escribe el segundo número")
 
